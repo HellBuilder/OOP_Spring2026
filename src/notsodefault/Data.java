315b8e9ef1d085e309e4d35068fad2b5e4c967e6
HellBuilder <bakhtybajzenis@gmail.com>
2026-02-11 02:54:44 +0500

    Create Data.java

---
+++ src/notsodefault/Data.java
@@ -0,0 +1,36 @@
+package notsodefault;
+
+public class Data {
+
+    private double sum;
+    private double max;
+    private int count;
+
+    public Data() {
+        sum = 0.0;
+        max = Double.NEGATIVE_INFINITY;
+        count = 0;
+    }
+
+    public void add(double value) {
+        sum += value;
+        if (value > max) {
+            max = value;
+        }
+        count++;
+    }
+
+    public double getAverage() {
+        if (count == 0) {
+            return 0.0;
+        }
+        return sum / count;
+    }
+
+    public double getMaximum() {
+        if (count == 0) {
+            return 0.0;
+        }
+        return max;
+    }
+}
